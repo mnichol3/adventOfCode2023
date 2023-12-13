6b8a3ab01c7fb00bbcf7ef3c2f8a1ad59e3b60ea
mnichol3 <nicholson.matt94@gmail.com>
2023-12-12 21:41:11 -0500

    I got so much cheese, I could open up a deli

--- day11/main.py
+++ day11/main.py
@@ -35,17 +35,12 @@ SAMPLE_INP_EXP = [
 ]
 
 
-def pretty_print(grid: list[list[str]]) -> None:
-    for x in grid:
-        print(x)
-
-
 def explode(grid: list[str]) -> list[list[str]]:
     return [list(x) for x in grid]
 
 
-def solve_1(grid: list[list[str]]) -> int:
-    """Solution to Part 1."""
+def solve(grid: list[list[str]], incr: int = 2) -> int:
+    """Solution to Parts 1 & 2"""
     visited = []
     distances = []
 
@@ -58,8 +53,8 @@ def solve_1(grid: list[list[str]]) -> int:
     for idx, coord in enumerate(coords):
         i, j = coord
 
-        i += len([x for x in empty_rows if x < i])
-        j += len([y for y in empty_cols if y < j])
+        i += len([x for x in empty_rows if x < i]) * (incr - 1)
+        j += len([y for y in empty_cols if y < j]) * (incr - 1)
 
         coords[idx] = (i, j, idx + 1)
 
@@ -67,34 +62,41 @@ def solve_1(grid: list[list[str]]) -> int:
         for dest in coords[idx:]:
             path = f'{origin[2]}-{dest[2]}'
             if path not in visited:
-                delta_i = abs(dest[0] - origin[0])
-                delta_j = abs(dest[1] - origin[1])
-
-                dist = delta_i + delta_j
-
-                distances.append(dist)
+                distances.append(
+                    abs(dest[0] - origin[0]) + abs(dest[1] - origin[1])
+                )
                 visited.append(path)
 
     return sum(distances)
 
 
-def solve_2(grid: list[list[str]]) -> int:
-    """Solution to Part 2."""
-    raise NotImplementedError
-
-
 def test_1() -> None:
+    """Simple unit test for Part 1 solution."""
     soln = solve_1(explode(SAMPLE_INP))
     assert soln == 374, f'{soln} != 374'
 
     print('Test 1 OK')
 
 
+def test_2() -> None:
+    """Simple unit test for Part 2 solution."""
+    inp = explode(SAMPLE_INP)
+    soln = solve_2(inp, incr=10)
+    assert soln == 1030, f'{soln} != 1030'
+
+    soln = solve_2(inp, incr=100)
+    assert soln == 8410, f'{soln} != 8410'
+
+    print('Test 2 OK')
+
+
 if __name__ == '__main__':
     inp = explode(Path(__file__).with_name('input.txt').read_text().splitlines())
 
-    #test_1()
-    soln = solve_1(inp)
+    # Part 1 solution
+    #soln = solve(inp)
+
+    # Part 2 solution
+    soln = solve(inp, incr=1000000)
 
     print(soln)
-
